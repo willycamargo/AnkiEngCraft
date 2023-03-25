d728b140f774de659408765cb34d0d8ee195a28f
Willy Camargo <camargowilly@gmail.com>
2023-03-25 19:56:11 -0300

    Refactor script to generate audio only if audio does not exist
    Added more comments and print statements

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
-import sys
+# This script creates Anki decks or CSV files from a CSV input file,
+# translating the sentences and generating audio using Azure Speech API.
 import os
 import csv
 import argparse
@@ -9,21 +10,28 @@ from googletrans import Translator
 from multiprocessing import Pool, cpu_count
 from azure.cognitiveservices.speech import SpeechConfig, SpeechSynthesizer, AudioConfig
 
+# Load environment variables from the .env file
 load_dotenv()
 
+# Get Azure Speech API credentials from environment variables
 speech_key = os.getenv('AZURE_SPEECH_KEY')
 service_region = os.getenv('AZURE_REGION')
 
+# Anki Static IDs
+anki_model_id = int(os.getenv('ANKI_MODEL_ID'))
+anki_deck_id = int(os.getenv('ANKI_DECK_ID'))
+
+# Reads CSV data from a file
 def read_csv(file_name):
-    print('Reading initial CSV file...')
+    print('Reading initial CSV file.')
     with open(file_name, 'r') as f:
         reader = csv.DictReader(f)
         csv_data = list(reader)
         return csv_data
 
-
+# Reads CSV data from a file
 def write_csv(file_name, cards):
-    print('Writing final CSV file...')
+    print('Writing final CSV file.')
     keys = cards[0].keys()
     with open(file_name, 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
@@ -31,11 +39,11 @@ def write_csv(file_name, cards):
         dict_writer.writerows(cards)
 
 def create_anki_deck(deck_name, cards):
-    print('Creating Anki deck...')
+    print('Creating Anki deck.')
 
     # Define the Anki model
     model = genanki.Model(
-        model_id=1607392319,
+        model_id=anki_model_id,
         name='English Vocab Model',
         fields=[
             {'name': 'Front'},
@@ -52,17 +60,11 @@ def create_anki_deck(deck_name, cards):
 
     # Create the deck
     deck = genanki.Deck(
-        deck_id=2059400110,
+        deck_id=anki_deck_id,
         name=deck_name)
 
-    # Add notes (cards) to the deck
-    for card in cards:
-        note = genanki.Note(
-            model=model,
-            fields=[card['Front'], card['Back'], card.get('Audio', '')])
-        deck.add_note(note)
-
     # Add notes (cards) to the deck and collect audio file paths
+    print('Adding notes to the Anki deck.')
     media_files = []
     for card in cards:
         note = genanki.Note(
@@ -91,22 +93,28 @@ def translate_sentence(sentence):
 
 
 def synthesize_speech(text, speech_config):
-    audio_config = AudioConfig(filename=f"output/audio/{text}.mp3")
-    synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-    synthesizer.speak_text(text)
+    audio_filename = f"output/audio/{text}.mp3"
+    print(f"Checking if audio file {audio_filename} exists.")
+    if not os.path.exists(audio_filename):
+        print(f"File {audio_filename} do not exists, creating file using Azure Speech API.")
+        audio_config = AudioConfig(filename=audio_filename)
+        synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+        synthesizer.speak_text(text)
     return f"[sound:{text}.mp3]"
 
 
 def create_cards_with_translation(sentences):
-    print('Initializing translation...')
+    print('Initializing translation.')
     with Pool(cpu_count()) as pool:
         cards = pool.map(partial(translate_sentence), sentences)
     return cards
 
 if __name__ == "__main__":
+    # Create necessary directories
     os.makedirs("output", exist_ok=True)
     os.makedirs("output/audio", exist_ok=True)
 
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description='Create Anki decks or CSV files from a CSV input file.')
     parser.add_argument('--filename', help='The input CSV file name')
     parser.add_argument('--output', choices=['anki', 'csv'], default='anki', help='The output format (default: anki)')
