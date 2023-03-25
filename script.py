293e4df1343ef57cc6b10b1792b09ae90546d730
Willy Camargo <camargowilly@gmail.com>
2023-03-25 12:21:05 -0300

    Refactor script to accept `<filename>` param and speed it up by using parallel processing

--- script.py
+++ script.py
@@ -1,43 +1,51 @@
-from googletrans import Translator
+import sys
 import csv
+from googletrans import Translator
+from multiprocessing import Pool, cpu_count
 
 
-# translated = translator.translate('hello', src='en', dest='pt')
-# print(translated)
+def read_csv(file_name):
+    print('Reading initial CSV file...')
+    with open(file_name, 'r') as f:
+        reader = csv.DictReader(f)
+        csv_data = list(reader)
+        return csv_data
 
-def read_csv(fileName):
-  print('Reading initial CSV file...')
-  with open(fileName, 'r') as f:
-    reader = csv.DictReader(f)
-    csvData = list(reader)
-    return csvData
 
-def write_csv(fileName, cards):
-  print('Writing final CSV file...')
-  keys = cards[0].keys()
-  with open(fileName, 'w', newline='')  as output_file:
-      dict_writer = csv.DictWriter(output_file, keys)
-      dict_writer.writeheader()
-      dict_writer.writerows(cards)
+def write_csv(file_name, cards):
+    print('Writing final CSV file...')
+    keys = cards[0].keys()
+    with open(file_name, 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(cards)
 
-def create_cards_with_translation(sentences):
-  print('Initializing translation..')
-  cards = []
-  translator = Translator()
-  
-  for sentence in sentences:
+
+def translate_sentence(sentence):
+    translator = Translator()
     print('Translating sentence: ' + sentence)
     translation = translator.translate(sentence, src='en', dest='pt')
-    currentCard = {
-      "Front": sentence,
-      "Back": translation.text
+    current_card = {
+        "Front": sentence,
+        "Back": translation.text
     }
-    cards.append(currentCard)
-  return cards
+    return current_card
 
-# Execution
-fileName = 'Anki Cards - English Vocab 05.csv'
-csvData = read_csv('input/' + fileName)
-sentences = [d['sentence'] for d in csvData]
-cards = create_cards_with_translation(sentences)
-write_csv('output/' + fileName, cards)
+
+def create_cards_with_translation(sentences):
+    print('Initializing translation...')
+    with Pool(cpu_count()) as pool:
+        cards = pool.map(translate_sentence, sentences)
+    return cards
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <filename>")
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+    csv_data = read_csv('input/' + file_name)
+    sentences = [d['Sentence'] for d in csv_data]
+    cards = create_cards_with_translation(sentences)
+    write_csv('output/' + file_name, cards)
